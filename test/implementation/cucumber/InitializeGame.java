217b060c71a395fa2f9675878822521eba22ffd1
Wohlfart Richárd <fargoricsi@gmail.com>
2023-05-29 19:42:52 +0200

    Game init test implemented
    Added a new cucumber test
    which initializes the game with 5 virologists

--- test/implementation/cucumber/InitializeGame.java
+++ test/implementation/cucumber/InitializeGame.java
@@ -1,25 +1,32 @@
 package implementation.cucumber;
 
+import static org.junit.jupiter.api.Assertions.*;
+
+import Control.LevelManager;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
+import View.GameFrame;
+
 public class InitializeGame {
 
-    @Given("the game board does not exist yet")
-    public void theGameBoardDoesNotExistYet() {
-    }
+    private GameFrame gameFrame;
 
-    @When("the one of the players presses start")
-    public void theOneOfThePlayersPressesStart() {
+    @When("one of the players presses start")
+    public void oneOfThePlayersPressesStart() {
+        gameFrame = new GameFrame();
+        gameFrame.getJMenuBar().getMenu(0).getItem(0).doClick();
     }
 
     @Then("The tiles get created")
     public void theTilesGetCreated() {
+        assertTrue(LevelManager.getInstance().getTiles().size() >= 5);
     }
 
     @And("the virologists are placed on the board")
     public void theVirologistsArePlacedOnTheBoard() {
+        assertEquals(LevelManager.getInstance().getVirologists().size(), 5);
     }
 }
--- test/resources/initialize-game.feature
+++ test/resources/initialize-game.feature
@@ -1,8 +1,7 @@
 Feature: Initialize a New Game
 
 @InitializeGame
-Scenario: Initialize the game board and the characters
-  Given the game board does not exist yet
-  When the one of the players presses start
+Scenario: Initialize the game board with five virologists
+  When one of the players presses start
   Then The tiles get created
   And the virologists are placed on the board
