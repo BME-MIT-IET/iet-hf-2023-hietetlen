3994d972f6bd229b4d44b1d1e693a53e5a3b7b05
Wohlfart Richárd <fargoricsi@gmail.com>
2023-05-29 19:27:06 +0200

    Added the cucumber folders and base classes

---
+++ test/implementation/cucumber/InitializeGame.java
@@ -0,0 +1,25 @@
+package implementation.cucumber;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class InitializeGame {
+
+    @Given("the game board does not exist yet")
+    public void theGameBoardDoesNotExistYet() {
+    }
+
+    @When("the one of the players presses start")
+    public void theOneOfThePlayersPressesStart() {
+    }
+
+    @Then("The tiles get created")
+    public void theTilesGetCreated() {
+    }
+
+    @And("the virologists are placed on the board")
+    public void theVirologistsArePlacedOnTheBoard() {
+    }
+}
