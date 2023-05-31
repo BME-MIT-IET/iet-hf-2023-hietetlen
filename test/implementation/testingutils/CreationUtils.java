eae6e6878702851eaad62c145991a17e0c7664c0
Wohlfart Richárd <fargoricsi@gmail.com>
2023-05-31 21:06:13 +0200

    Added a utils class for making tests easier

---
+++ test/implementation/testingutils/CreationUtils.java
@@ -0,0 +1,22 @@
+package implementation.testingutils;
+
+import Model.Effect;
+import Model.Effects.MemoryLoss;
+import Model.GeneticCode;
+import Model.SubstanceContainer;
+import Model.Tiles.Tile;
+import Model.Virologist;
+
+import java.util.function.Supplier;
+
+public class CreationUtils {
+    public static Virologist CreateVirologistWithVirus(Tile tile, Supplier<Effect> effectSupplier, boolean isVirus) {
+        if (tile == null) tile = new Tile();
+        if (effectSupplier == null) effectSupplier = () -> new MemoryLoss();
+        var virologist = new Virologist(tile);
+        var geneticCode = new GeneticCode(effectSupplier, new SubstanceContainer(0,0), 10);
+        virologist.collectGeneticCode(geneticCode);
+        virologist.makeAgent(isVirus, geneticCode);
+        return virologist;
+    }
+}
